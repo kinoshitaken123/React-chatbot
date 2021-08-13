33433717941ba4d46300825279ef293bac1f5f72
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-13 15:51:52 +0900

    繰り返し再利用できる関数コンポーネント

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from './dataset'
 import './assets/styles/style.css'
+import { AnswersList } from './components';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,10 +14,25 @@ export default class App extends React.Component {
      open: false               // 問い合わせフォーム用モーダルの開閉を管理
     };
   }
+
+  initAnswer = () =>{
+    const initDataset = this.state.dateset[this.state.currentId];
+    const initAnswers  = initDataset.answers;
+
+    this.setState( {
+      answers: initAnswers
+    })
+  }
+
+  componentDidMount() {
+    this.initAnswer()
+  }
+
   render () {
   return (
     <section className ="c-section">
      <div className="c-box">
+       <AnswersList answers={this.state.answers} />
      </div>
     </section>
   );
