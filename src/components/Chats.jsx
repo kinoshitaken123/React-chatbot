c356075492adba820dc18b5f232aa010f417db20
“kinoshitaken123” <k1k7s@yahoo.co.jp>
2021-08-14 10:46:01 +0900

    [empty]

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import {Chat} from "./index";
+import { Chat } from './index';
 
 const useStyles = makeStyles((theme) => ({
     root: {
