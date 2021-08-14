aa51445a67a81c24cb4b6f38b4dd1fbd85584501
“kinoshitaken123” <k1k7s@yahoo.co.jp>
2021-08-14 16:09:04 +0900

    [lifesicle]

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,17 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
          break;
+
+      // リンクが選択された時
+      case /^https:*/.test(nextQuestionId):
+        const a  = document.createElement('a');
+        a.href   = nextQuestionId;
+        a.target = '_blanck';
+        a.click();
+        break;
+        
       default:
         const chats = this.state.chats;
         chats.push({
@@ -47,7 +56,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000);
         break;
     }
   }
@@ -55,6 +64,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.clientHeight;
+    }
   }
 
   render () {
